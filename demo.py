382d640b9fe49c2c042bc421dcb6d4a496e74151
Sakshi Jain <sakjai04@student.ubc.ca>
2020-11-20 20:40:39 +0530

    updated file second time

--- demo.py
+++ demo.py
@@ -2,16 +2,23 @@
 # date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg123> ]
+Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg123>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-[<arg123> ]          Takes any value (this is a optional positional argument)  
+[<arg123>]        Takes any value (this is a optional positional argument)  
 """
 
 from docopt import docopt
-opt = docopt(__doc__)
-print(opt)
-print(type(opt))
+opt = docopt(__doc__)  
+
+def main(opt1, opt2, opt3, opt4):
+
+
+    print(opt)
+    print(type(opt)) 
+
+if __name__ == "__main__":
+    main(opt["<arg1>"], opt["--arg2"], opt["--arg3"], opt["<arg123>"])
